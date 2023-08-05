c2c725e057c09ecf14f2ec870e8813028944d911
jaymin1088 <139453693+jaymin1088@users.noreply.github.com>
2023-08-05 13:24:19 +0530

    delete all files for modify

--- emprestapi/src/main/java/com/glearning/emps/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.glearning.emps.service;
-
-import com.glearning.emps.model.Role;
-
-public interface RoleService {
-	Role save(Role role);
-
-	Role findByName(String name);
-}
--- emprestapi/src/main/java/com/glearning/emps/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.glearning.emps.service;
-
-import com.glearning.emps.model.User;
-
-public interface UserService {
-	User save(User user);
-
-	User findByUsername(String username);
-}
